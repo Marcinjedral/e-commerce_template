ddb831b65d091f8018647a0c0b1bbdfd08615ed3
Marcinjedral <info.marcinjedral@gmail.com>
2019-05-07 00:20:54 +0200

    e-commerce template

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+let gulp = require('gulp');
+let gulpSass = require('gulp-sass');
+
+gulp.task('buildcss', function () {
+    return gulp.src('./dev-assets/style.scss')
+        .pipe(gulpSass())
+        .pipe(gulp.dest('./prod-assets'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./dev-assets/**/*.scss', gulp.series('buildcss'));
+});
